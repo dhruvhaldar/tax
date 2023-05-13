460ffbfd85f8fd489758701fb922c736e26daeb7
dhruvhaldar <haldardhruv@gmail.com>
2023-05-13 18:52:08 +0530

    test: update test_itr.py

--- test_itr.py
+++ test_itr.py
@@ -4,9 +4,9 @@ from itr import income_from_hp, tax_calculation
 
 class TestTaxCalculation(unittest.TestCase):
     def test_tax_calculation(self):
-        self.assertEqual(tax_calculation(2022, 2023, 350000, 1), None)
-        self.assertEqual(tax_calculation(2022, 2023, 1000, 2), None)
-        self.assertEqual(tax_calculation(2022, 2023, 8000000, 2), None)
+        self.assertEqual(tax_calculation(2022, 2023, 1), None)
+        self.assertEqual(tax_calculation(2022, 2023, 2), None)
+        self.assertEqual(tax_calculation(2022, 2023, 2), None)
       
 if __name__ == '__main__':
     unittest.main()
