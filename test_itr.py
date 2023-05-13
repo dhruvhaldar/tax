ba51700a38b5c3571316aee79c403444fe3bda42
dhruvhaldar <haldardhruv@gmail.com>
2023-05-13 13:31:34 +0530

    test: update test_itr.py

--- test_itr.py
+++ test_itr.py
@@ -1,3 +1,4 @@
+import sys
 import unittest
 
 from itr import calculate_income_tax_new, calculate_income_tax_old
@@ -26,4 +27,14 @@ class TestCalculateIncomeTaxOld(unittest.TestCase):
         self.assertEqual(calculate_income_tax_new(2000000), 470000)
 
 if __name__ == '__main__':
-    unittest.main()
+    regime = sys.argv[1] if len(sys.argv) > 1 else 'Old'
+
+    if regime.lower() == 'old':
+        suite = unittest.TestLoader().loadTestsFromTestCase(TestCalculateIncomeTaxOld)
+    elif regime.lower() == 'new':
+        suite = unittest.TestLoader().loadTestsFromTestCase(TestCalculateIncomeTaxNew)
+    else:
+        print("Invalid regime specified")
+        sys.exit(1)
+
+    unittest.TextTestRunner().run(suite)
