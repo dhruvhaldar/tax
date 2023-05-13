bd83a63ba851ff45466a277add59f8fb42cba473
dhruvhaldar <haldardhruv@gmail.com>
2023-05-13 19:09:49 +0530

    test: update test_itr.py

--- test_itr.py
+++ test_itr.py
@@ -2,7 +2,7 @@ import unittest
 from io import StringIO
 from unittest.mock import patch
 
-from itr import calculate_income_from_salary_tax_old, tax_calculation
+from itr import calculate_income_from_salary_tax_new, calculate_income_from_salary_tax_old, tax_calculation
 
 
 class TestTaxCalculation(unittest.TestCase):
@@ -21,8 +21,44 @@ class TestCalculateIncomeFromSalaryTaxOld(unittest.TestCase):
     def test_calculate_income_from_salary_tax_old(self):
         total_income_from_salary = 500000
         expected_output = 14300.0
-        self.assertAlmostEqual(calculate_income_from_salary_tax_old(total_income_from_salary), expected_output, delta=0.001)
+        self.assertAlmostEqual(calculate_income_from_salary_tax_old(total_income_from_salary), expected_output)
+        
+    def test_calculate_income_from_salary_tax_slab1(self):
+        total_income_from_salary = 150000
+        expected_output = 0
+        self.assertAlmostEqual(calculate_income_from_salary_tax_old(total_income_from_salary), expected_output)
+        
+    def test_calculate_income_from_salary_tax_slab2(self):
+        total_income_from_salary = 350000
+        expected_output = 5720
+        self.assertAlmostEqual(calculate_income_from_salary_tax_old(total_income_from_salary), expected_output)
+
+class TestCalculateIncomeFromSalaryTaxNew(unittest.TestCase):
+
+    # def test_tax_below_slab_one(self):
+    #     self.assertEqual(calculate_income_from_salary_tax_new(200000), 0)
+    
+    # def test_tax_in_slab_one(self):
+    #     self.assertEqual(calculate_income_from_salary_tax_new(300000), 2500)
+    
+    # def test_tax_in_slab_two(self):
+    #     self.assertEqual(calculate_income_from_salary_tax_new(600000), 45000)
+    
+    # def test_tax_in_slab_three(self):
+    #     self.assertEqual(calculate_income_from_salary_tax_new(800000), 105000)
+    
+    # def test_tax_in_slab_four(self):
+    #     self.assertEqual(calculate_income_from_salary_tax_new(1200000), 225000)
+    
+    # def test_tax_in_slab_five(self):
+    #     self.assertEqual(calculate_income_from_salary_tax_new(1400000), 325000)
+    
+    # def test_tax_above_slab_six(self):
+    #     self.assertEqual(calculate_income_from_salary_tax_new(2000000), 525000)
+    
+    # def test_tax_with_surcharge(self):
+    #     self.assertEqual(calculate_income_from_salary_tax_new(10000000), 2966250)
 
 
-if __name__ == '__main__':
-    unittest.main()
+    if __name__ == '__main__':
+        unittest.main()
